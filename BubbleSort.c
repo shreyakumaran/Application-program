41a26308956715afc53192e910378aa128ea2a78
Shreya Kumaran <119963396+shreyakumaran@users.noreply.github.com>
2023-10-04 09:04:01 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -22,3 +22,12 @@ int main()
         printf("%d ", arr[i]);
     return 0;
 }
+ALGORITHM
+Step1:Starting with the first element(index = 0), compare the current element with the next element of the array.
+Step2:If the current element is greater than the next element of the array, swap them.
+Step3:If the current element is less than the next element, move to the next element. Repeat Step 1.
+
+TIME COMPLEXITY
+Best Case:O(n)
+Average Case:O(n2)
+Worst Case:O(n2)
