b565a717129df2187efb443c58632b005f0f5ebc
Shreya Kumaran <119963396+shreyakumaran@users.noreply.github.com>
2023-10-04 09:07:16 +0530

    Update Prims algorithm.c

--- Prims algorithm.c
+++ Prims algorithm.c
@@ -101,3 +101,10 @@ int main()
 
 	return 0;
 }
+ALGORITHM:
+Step 1: Select a starting vertex  
+Step 2: Repeat Steps 3 and 4 until there are fringe vertices  
+Step 3: Select an edge 'e' connecting the tree vertex and fringe vertex that has minimum weight  
+Step 4: Add the selected edge and the vertex to the minimum spanning tree T  
+[END OF LOOP]  
+Step 5: EXIT  
